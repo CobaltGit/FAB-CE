b75f75af69ea8039c739176c088675bfb53e925d
CobaltGit <thealmightygamer212@gmail.com>
2021-06-26 20:55:56 +0100

    Entrypoint no longer needed

--- src/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-mv /usr/src/fab/data/settings.json /usr/src/fab/data/settings-template.json
-envsubst < /usr/src/fab/data/settings-template.json > /usr/src/fab/data/settings.json
-node /usr/src/fab/bot.js
