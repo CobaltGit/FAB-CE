77a2858ceec76d73d183faea78f1b2df887f200d
CobaltGit <thealmightygamer212@gmail.com>
2021-07-10 19:50:17 +0100

    Fix???

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,5 +2,6 @@
 
 cd /usr/src/fab
 envsubst < ./data/settings.json > ./data/settings-postenv.json
-mv ./data/settings-postenv.json ./data/settings.json
+rm ./data/settings.json
+mv ./data/settings-postenv.json
 node bot.js
